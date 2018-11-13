c130f17a5db30762cd339808b83b0e87ec7bce2a
Marco Tran <mtran0132@gmail.com>
2018-11-12 19:49:25 -0800

    Added comments and other things

--- src/com/company/UDPClient.java
+++ src/com/company/UDPClient.java
@@ -1,3 +1,8 @@
+/*
+Created By Group 2
+Marco Tran
+A command line program that launches a GUI
+ */
 package com.company;
 
 import javax.swing.*;
@@ -6,7 +11,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
-import java.util.InputMismatchException;
 
 public class UDPClient {
     private byte[] message;
@@ -16,6 +20,7 @@ public class UDPClient {
     static JFrame frame;
     static GraphicsConfiguration gc;
 
+    // Creates a new Client Object with a message, address and port
     public UDPClient(byte[] message, InetAddress address, int port ) throws SocketException{
         this.message = message;
         this.address = address;
@@ -23,63 +28,99 @@ public class UDPClient {
         this.socket = new DatagramSocket();
     }
 
-    public void send() throws IOException{
-        DatagramPacket request = new DatagramPacket(this.message, this.message.length, this.address, this.port);
-        this.socket.send(request);
-        System.out.println("------ CLIENT ------");
-        System.out.println("Message Sent");
-    }
-
-    public void  receive() throws IOException{
-        byte[] buffer = new byte[1000];
-        DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-        this.socket.receive(reply);
-        System.out.println("------ CLIENT ------");
-        System.out.println("Message received from server");
-        System.out.println("Reply: " + new String(reply.getData()));
-    }
-
-    public byte[] getMessage() {
-        return message;
-    }
-
+    // Returns the client's socket
     public DatagramSocket getSocket(){
         return this.socket;
     }
 
+    // Sends the message to the server
+
+    public void send() throws IOException{
+
+        if(!this.socket.isClosed()){
+            // Creates the packet
+            DatagramPacket request = new DatagramPacket(this.message, this.message.length, this.address, this.port);
+            // Sends the packet
+            // Information is within the packet
+            this.socket.send(request);
+
+            // Console output
+            System.out.println("------ CLIENT ------");
+            System.out.println("Message Sent");
+        }
+    }
+    // Receives message from server
+
+    public void  receive() throws IOException{
+        if(!this.socket.isClosed()){
+
+            byte[] buffer = new byte[1000];
+
+            // Creates the reply
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+
+            // Receives the reply
+            this.socket.receive(reply);
+
+            // Console output
+            System.out.println("------ CLIENT ------");
+            System.out.println("Message received from server");
+
+            // Displays what the server sent back
+            System.out.println("Reply: " + new String(reply.getData()));
+        }
+    }
+
+    // Creates a GUI for user to input messages to send over
+    // Centered and a third of your screen
     public void createGUI(){
 
+        // Gets the screens dimensions
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         frame = new JFrame(gc);
         frame.setLayout(new GridLayout(6,2));
         frame.setTitle("CLIENT");
 
+        // Create Labels
         JLabel portLabel = new JLabel();
-        portLabel.setText("<html><h2>Using Port Number: " + this.port + "</h2></html>");
+        portLabel.setText("<html><h2>" + this.address.getHostAddress() + "<br>Listening on Port Number: " + this.port + "</h2></html>");
         portLabel.setBounds(10,10,100,100);
-        frame.getContentPane().add(portLabel);
 
+        // Create Labels
         JLabel messageLabel = new JLabel();
         messageLabel.setText("<html><p>Enter message to send: </p></html>");
-        frame.getContentPane().add(messageLabel);
 
+        // Create the text field for the user to input
         JTextField messageField = new JTextField();
         messageField.setBounds(10,10,100,100);
+
+        // Add the labels to GUI
+        frame.getContentPane().add(portLabel);
+        frame.getContentPane().add(messageLabel);
         frame.getContentPane().add(messageField);
 
+        // Create the button to send
         JButton send = new JButton("SEND");
-        send.setBounds(100,100,140,40);
+        send.setBounds(100,100,120,40);
+
+        // Add an actionlister to the button
+        // Whenever the button is pressed, do this anonymous function
         send.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
                 try{
+
+                    // Prevents empty messages to be sent
                     if(!messageField.getText().isEmpty()) {
                         UDPClient temp = new UDPClient(messageField.getText().getBytes(), address, port);
                         temp.send();
                         temp.receive();
+                        // Clears the text box
                         messageField.setText("");
                     }
                 }
+
+                // Just catching my exceptions
                 catch (NumberFormatException nfe){ }
                 catch (UnknownHostException uke){ }
                 catch (SocketException se){ }
@@ -94,20 +135,33 @@ public class UDPClient {
                 }
             }
         });
+
+        // Add the button to the GUI
         frame.getContentPane().add(send);
 
+        // Set the size to a third of your screen
         frame.setSize(dim.width/3,dim.height/3);
+        // Center the GUI
         frame.setLocationRelativeTo(null);
+        // Exit the program when the GUI closes
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // Display the GUI
         frame.setVisible(true);
     }
 
+    // ENTRY POINT
     public static void main(String[] args){
         UDPClient client = null;
         try{
+            // Creates a client object
+            // (message, address, port)
             client = new UDPClient(args[0].getBytes(), InetAddress.getByName(args[1]), Integer.parseInt(args[2]));
+
+            // Creates the GUI
             client.createGUI();
+            // Send the message
             client.send();
+            // Receive the reply
             client.receive();
         }
         catch (NumberFormatException e){
@@ -129,9 +183,7 @@ public class UDPClient {
                     client.getSocket().close();
                 }
             }
-            catch(NullPointerException e){
-
-            }
+            catch(NullPointerException e){ }
         }
     }
 }
--- src/com/company/UDPServer.java
+++ src/com/company/UDPServer.java
@@ -1,9 +1,13 @@
+/*
+Created By Group 2
+Marco Tran
+A command line program that launches a GUI
+ */
+
 package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.*;
 import java.net.*;
 
@@ -11,96 +15,117 @@ public class UDPServer extends JFrame{
 
     private DatagramSocket socket;
     private DatagramPacket request;
-    private DatagramPacket reply;
     private int port;
     static GraphicsConfiguration gc;
     private JFrame frame;
-    private boolean running;
+    private InetAddress address;
 
-    public UDPServer(int port) throws SocketException{
+    // Constructor
+    // Given an address and port
+    // Create a new socket
+    public UDPServer(InetAddress address, int port) throws SocketException{
         this.port = port;
-        this.socket = new DatagramSocket(this.port);
-        this.running = true;
+        this.address = address;
+        this.socket = new DatagramSocket(port, address);
     }
 
+    // Returns the socket of the Server
     public DatagramSocket getSocket(){
         return this.socket;
     }
 
-    public boolean isRunning(){
-        return this.running;
-    }
-
-    public void setRunning(boolean bool){
-        this.running = bool;
-    }
-
+    // Returns the port of the Server
     public int getPort(){
         return this.port;
     }
 
+    // Using a socket, the server listens for incoming messages on a specific port
     public void listen() throws IOException{
         while(true){
             byte[] buffer = new byte[1000];
             DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-            this.request = request;
+
+            // Receives a DatagramPacket from this socket
+            // This method blocks until a Datagram is received
             this.socket.receive(request);
+            this.request = request;
+
+            // Server crafts a reply
             DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
-            this.reply = reply;
+            // Sends the reply
+            // Using the information from the packet
             this.socket.send(reply);
             addEntry();
         }
     }
 
+    // Creates a dynamic GUI
+    // Centered and a third of your screen
     public void createGUI(){
 
+        // Get the dimensions of the screen
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         frame = new JFrame(gc);
         frame.setLayout(new GridLayout(0,2));
         frame.setTitle("SERVER");
 
+        // Creating Labels
         JLabel portLabel = new JLabel();
-        portLabel.setText("<html><h2>Listening on Port Number: " + this.port + "</h2></html>");
+        portLabel.setText("<html><h2>" + this.address.getHostAddress() +"<br>Listening on Port Number: " + this.port + "</h2></html>");
         portLabel.setBounds(10,10,100,100);
-        frame.getContentPane().add(portLabel);
 
+        //Creating Labels
         JLabel name = new JLabel();
         name.setText("<html><h3>Group 2: Marco Tran</h3></html>");
         name.setBounds(10,10,100,100);
+
+        // Add the Labels
+        frame.getContentPane().add(portLabel);
         frame.getContentPane().add(name);
 
+        // Set the GUI to a third of your screen
         frame.setSize(dim.width/3,dim.height/3);
+        // Center the GUI
         frame.setLocationRelativeTo(null);
+        // Stop the program once you exit the GUI
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // Display the GUI
         frame.setVisible(true);
-
     }
 
+    // Adds an entry to the GUI everytime a request comes in.
     public void addEntry(){
         JLabel packetLabel = new JLabel();
         packetLabel.setText("<html><p>REQUEST: " + new String(this.request.getData()) +"</p></html>");
         this.frame.getContentPane().add(packetLabel);
     }
 
+    // ENTRY POINT
     public static void main(String[] args){
 
         UDPServer server = null;
+
         try{
-            server = new UDPServer(Integer.parseInt(args[0]));
+            // Create a server with command line arguments
+            // Internet Address, Port Number
+            server = new UDPServer(InetAddress.getByName(args[0]),Integer.parseInt(args[1]));
+
+            // Makes the GUI popup
             server.createGUI();
             System.out.println("------ SERVER ------");
             System.out.println("Server listening on port: " + server.getPort());
-            while(server.isRunning()){
-                server.listen();
-            }
+
+            // Server starts listening
+            server.listen();
         }
+
+        // Fail cases taken care of(?)
         catch (NumberFormatException e){
             System.out.println("Invalid Port, use integers\n" + e.getMessage());
         }
         catch (SocketException e){
             System.out.println("Socket\n" + e.getMessage());
         }
-
         catch (IOException e){
             System.out.println("IO\n" + e.getMessage());
         }
@@ -110,9 +135,7 @@ public class UDPServer extends JFrame{
                     server.getSocket().close();
                 }
             }
-            catch(NullPointerException e){
-
-            }
+            catch(NullPointerException e){ }
         }
     }
 }
