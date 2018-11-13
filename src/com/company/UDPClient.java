7e8217a82de0b7a7399ea3fde1cb3e5ca66abddd
Marco Tran <mtran0132@gmail.com>
2018-11-12 18:51:02 -0800

    Added GUI

--- src/com/company/UDPClient.java
+++ src/com/company/UDPClient.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
 import java.util.InputMismatchException;
@@ -9,6 +13,8 @@ public class UDPClient {
     private InetAddress address;
     private int port;
     private DatagramSocket socket;
+    static JFrame frame;
+    static GraphicsConfiguration gc;
 
     public UDPClient(byte[] message, InetAddress address, int port ) throws SocketException{
         this.message = message;
@@ -19,7 +25,7 @@ public class UDPClient {
 
     public void send() throws IOException{
         DatagramPacket request = new DatagramPacket(this.message, this.message.length, this.address, this.port);
-        socket.send(request);
+        this.socket.send(request);
         System.out.println("------ CLIENT ------");
         System.out.println("Message Sent");
     }
@@ -27,21 +33,80 @@ public class UDPClient {
     public void  receive() throws IOException{
         byte[] buffer = new byte[1000];
         DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-        socket.receive(reply);
+        this.socket.receive(reply);
         System.out.println("------ CLIENT ------");
-        System.out.println("Message received");
+        System.out.println("Message received from server");
         System.out.println("Reply: " + new String(reply.getData()));
+    }
+
+    public byte[] getMessage() {
+        return message;
     }
 
     public DatagramSocket getSocket(){
         return this.socket;
     }
 
+    public void createGUI(){
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame = new JFrame(gc);
+        frame.setLayout(new GridLayout(6,2));
+        frame.setTitle("CLIENT");
+
+        JLabel portLabel = new JLabel();
+        portLabel.setText("<html><h2>Using Port Number: " + this.port + "</h2></html>");
+        portLabel.setBounds(10,10,100,100);
+        frame.getContentPane().add(portLabel);
+
+        JLabel messageLabel = new JLabel();
+        messageLabel.setText("<html><p>Enter message to send: </p></html>");
+        frame.getContentPane().add(messageLabel);
+
+        JTextField messageField = new JTextField();
+        messageField.setBounds(10,10,100,100);
+        frame.getContentPane().add(messageField);
+
+        JButton send = new JButton("SEND");
+        send.setBounds(100,100,140,40);
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                try{
+                    if(!messageField.getText().isEmpty()) {
+                        UDPClient temp = new UDPClient(messageField.getText().getBytes(), address, port);
+                        temp.send();
+                        temp.receive();
+                        messageField.setText("");
+                    }
+                }
+                catch (NumberFormatException nfe){ }
+                catch (UnknownHostException uke){ }
+                catch (SocketException se){ }
+                catch (IOException ioe){ }
+                finally{
+                    try{
+                        if(getSocket() != null){
+                            getSocket().close();
+                        }
+                    }
+                    catch(NullPointerException npe){ }
+                }
+            }
+        });
+        frame.getContentPane().add(send);
+
+        frame.setSize(dim.width/3,dim.height/3);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args){
         UDPClient client = null;
-
         try{
             client = new UDPClient(args[0].getBytes(), InetAddress.getByName(args[1]), Integer.parseInt(args[2]));
+            client.createGUI();
             client.send();
             client.receive();
         }
--- src/com/company/UDPServer.java
+++ src/com/company/UDPServer.java
@@ -1,21 +1,38 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.*;
 import java.net.*;
-import java.util.InputMismatchException;
 
-public class UDPServer {
+public class UDPServer extends JFrame{
 
     private DatagramSocket socket;
+    private DatagramPacket request;
+    private DatagramPacket reply;
     private int port;
+    static GraphicsConfiguration gc;
+    private JFrame frame;
+    private boolean running;
 
     public UDPServer(int port) throws SocketException{
         this.port = port;
         this.socket = new DatagramSocket(this.port);
+        this.running = true;
     }
 
     public DatagramSocket getSocket(){
         return this.socket;
+    }
+
+    public boolean isRunning(){
+        return this.running;
+    }
+
+    public void setRunning(boolean bool){
+        this.running = bool;
     }
 
     public int getPort(){
@@ -26,19 +43,54 @@ public class UDPServer {
         while(true){
             byte[] buffer = new byte[1000];
             DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+            this.request = request;
             this.socket.receive(request);
             DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
+            this.reply = reply;
             this.socket.send(reply);
+            addEntry();
         }
     }
 
+    public void createGUI(){
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame = new JFrame(gc);
+        frame.setLayout(new GridLayout(0,2));
+        frame.setTitle("SERVER");
+
+        JLabel portLabel = new JLabel();
+        portLabel.setText("<html><h2>Listening on Port Number: " + this.port + "</h2></html>");
+        portLabel.setBounds(10,10,100,100);
+        frame.getContentPane().add(portLabel);
+
+        JLabel name = new JLabel();
+        name.setText("<html><h3>Group 2: Marco Tran</h3></html>");
+        name.setBounds(10,10,100,100);
+        frame.getContentPane().add(name);
+
+        frame.setSize(dim.width/3,dim.height/3);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+    public void addEntry(){
+        JLabel packetLabel = new JLabel();
+        packetLabel.setText("<html><p>REQUEST: " + new String(this.request.getData()) +"</p></html>");
+        this.frame.getContentPane().add(packetLabel);
+    }
+
     public static void main(String[] args){
+
         UDPServer server = null;
         try{
             server = new UDPServer(Integer.parseInt(args[0]));
+            server.createGUI();
             System.out.println("------ SERVER ------");
             System.out.println("Server listening on port: " + server.getPort());
-            while(true){
+            while(server.isRunning()){
                 server.listen();
             }
         }
